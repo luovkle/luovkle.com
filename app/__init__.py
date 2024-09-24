a35d5a1b119070dca6f01292cb340d214ac1b52c
luovkle <roberto.silva.dev@protonmail.com>
2024-09-23 21:51:25 -0600

    ✨ Add error pages

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
 from .views import bp as views_bp
+
+
+def page_not_found(_):
+    return render_template("404.html"), 404
+
+
+def internal_server_error(_):
+    return render_template("500.html"), 500
 
 
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(views_bp)
+    app.register_error_handler(404, page_not_found)
+    app.register_error_handler(500, internal_server_error)
     return app
