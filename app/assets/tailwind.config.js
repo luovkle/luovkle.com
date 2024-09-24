f29f407fea1996ba2362920230515374c08cc96b
luovkle <roberto.silva.dev@protonmail.com>
2024-09-23 20:52:33 -0600

    🔧 Add tailwind config

---
+++ app/assets/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/templates/**/*.html", "./app/sources/**/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
