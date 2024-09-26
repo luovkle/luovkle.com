a57b603fc24bbf8994f81ab7c75607a119879e9f
luovkle <roberto.silva.dev@protonmail.com>
2024-09-25 23:04:59 -0600

    🍱 Add icons

--- app/assets/tailwind.config.js
+++ app/assets/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/templates/**/*.html", "./app/sources/**/*.html"],
+  content: ["app/templates/**/*.{html,svg}", "app/sources/**/*.html"],
   theme: {
     extend: {},
   },
