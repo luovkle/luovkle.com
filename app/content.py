1139d4ced224246da1a329ffc5ed4914b89659e0
luovkle <roberto.silva.dev@protonmail.com>
2024-10-02 15:31:19 -0600

    ⚡️ Improve performance by caching content

--- app/content.py
+++ app/content.py
@@ -1,8 +1,11 @@
+from functools import cache
+
 from flask import g
 
 from .loader import get_content_data
 
 
+@cache
 def get_content():
     if "content" not in g:
         g.content = get_content_data()
