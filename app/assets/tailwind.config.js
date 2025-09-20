304cc3f09f1d2ba2fedadf4fb96abd2979280bff
luovkle <roberto.silva.dev@protonmail.com>
2025-09-20 15:03:43 -0600

    🔧 Update TailwindCSS configuration from `v3` to `v4`

--- app/assets/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["app/templates/**/*.{html,svg}"],
-  safelist: ["text-sky-500", "font-bold"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
