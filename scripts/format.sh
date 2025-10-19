b40ae5fb6d3205707563e56d37fd663843ce2737
luovkle <roberto.silva.dev@protonmail.com>
2025-10-19 17:05:07 -0600

    ♻️ Replace black, flake8, isort, and autoflake with ruff

--- scripts/format.sh
+++ scripts/format.sh
@@ -3,8 +3,7 @@
 set -e
 set -x
 
-.venv/bin/autoflake --in-place .
-.venv/bin/black .
-.venv/bin/isort .
+.venv/bin/ruff check app --fix
+.venv/bin/ruff format app
 .venv/bin/djlint --profile=jinja --reformat --quiet --indent 2 \
   --preserve-blank-lines --close-void-tags --max-line-length 88 app/templates/
--- scripts/lint.sh
+++ scripts/lint.sh
@@ -3,9 +3,8 @@
 set -e
 set -x
 
-.venv/bin/black --check .
-.venv/bin/isort --check-only .
-.venv/bin/flake8 .
+.venv/bin/ruff check app
+.venv/bin/ruff format app --check
 .venv/bin/djlint --profile=jinja --check --indent 2 --preserve-blank-lines \
   --close-void-tags --max-line-length 88 app/templates/
 .venv/bin/djlint --lint app/templates/
